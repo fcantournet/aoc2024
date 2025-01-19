9fab7383aa929dde6d86dd9fb08040aaef2f21df
felix <Flix Cantournet>
2025-01-19 21:19:45 +0100

    day19 part2, actually 2 memoes was stupid

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use itertools::Itertools;
 
@@ -7,38 +7,45 @@ advent_of_code::solution!(19);
 pub fn part_one(input: &str) -> Option<usize> {
     let (towels, patterns) = parse_input(input);
 
-    let mut memo = HashSet::new();
     let mut memo_neg = HashSet::new();
     let mut solved = 0;
     for pattern in patterns.iter() {
-        if solves(pattern, &towels, &mut memo, &mut memo_neg) {
+        if solves(pattern, &towels, &mut memo_neg) {
             solved += 1;
         }
-        println!("{}", solved);
     }
-    // println!("{:#?}", memo);
+    Some(solved)
+    // part_one_factored(input)
+}
+
+fn part_one_factored(input: &str) -> Option<usize> {
+    let (towels, patterns) = parse_input(input);
+
+    let mut memo = HashMap::new();
+    let mut solved = 0;
+    for pattern in patterns.iter() {
+        if counts(pattern, &towels, &mut memo, true) > 0 {
+            solved += 1;
+        }
+    }
     Some(solved)
 }
 
-fn solves(
-    pattern: &str,
-    towels: &Vec<&str>,
-    memo: &mut HashSet<String>,
-    memoneg: &mut HashSet<String>,
-) -> bool {
-    if memo.contains(pattern) || pattern.len() == 0 {
+fn solves(pattern: &str, towels: &Vec<&str>, memoneg: &mut HashSet<String>) -> bool {
+    if pattern.len() == 0 {
         return true;
     }
+
     if memoneg.contains(pattern) {
         return false;
     }
 
     for t in towels.into_iter() {
         if let Some(remaining) = pattern.strip_suffix(t) {
-            println!("{} matched {}: remaing {}", pattern, t, remaining);
-            let solves = solves(remaining, towels, memo, memoneg);
+            // println!("{} matched {}: remaing {}", pattern, t, remaining);
+            let solves = solves(remaining, towels, memoneg);
             if solves {
-                memo.insert(pattern.to_string());
+                // memo.insert(pattern.to_string());
                 return true;
             }
         }
@@ -47,8 +54,41 @@ fn solves(
     false
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
-    None
+fn counts(
+    pattern: &str,
+    towels: &Vec<&str>,
+    memo: &mut HashMap<String, usize>,
+    short_circuit: bool,
+) -> usize {
+    if pattern.len() == 0 {
+        return 1;
+    }
+    if let Some(count) = memo.get(pattern) {
+        return *count;
+    }
+
+    let mut count = 0;
+    for t in towels.into_iter() {
+        if let Some(remaining) = pattern.strip_suffix(t) {
+            count += counts(remaining, towels, memo, short_circuit);
+            if short_circuit && count > 0 {
+                return count;
+            }
+        }
+    }
+    memo.insert(pattern.to_string(), count);
+    count
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let (towels, patterns) = parse_input(input);
+
+    let mut memo = HashMap::new();
+    let mut solved = 0;
+    for pattern in patterns.iter() {
+        solved += counts(pattern, &towels, &mut memo, false);
+    }
+    Some(solved)
 }
 
 fn parse_input(input: &str) -> (Vec<&str>, Vec<&str>) {
@@ -77,6 +117,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(16));
     }
 }
